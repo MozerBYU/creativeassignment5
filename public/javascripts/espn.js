770282fae289272852f85612f791cf3377462b40
MozerBYU <nathanmozer23@gmail.com>
2018-11-13 04:28:01 +0000

    Updated 11/12 9:27p by Nathan

--- public/javascripts/espn.js
+++ public/javascripts/espn.js
@@ -1,3 +1,14 @@
 //Stuff to do
 //Write angular to send out 2 teams specified in the html
 //select the winning team and put it in a new array of teams for the next round
+
+var app = angular.module('myTourneyApp', []);
+    app.controller('addCtrl', function($scope) {
+        
+    });
+    app.controller('showCtrl', function($scope) {
+        
+    });
+    app.controller('tourneyCtrl', function($scope) {
+        
+    });
